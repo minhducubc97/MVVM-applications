5d63748e53085f2b79ace9e89707e2b4eb3a9239
Duc Nguyen <31971333+minhducubc97@users.noreply.github.com>
2018-06-13 15:39:39 -0700

    Add files via upload

---
+++ CalculatorMiniProject/CalculatorMiniProject/Model/CalculatorModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace CalculatorMiniProject.Model
+{
+    public class CalculatorModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        private int firstNumber;
+        private int secondNumber;
+
+        public int FirstNumber
+        {
+            get
+            {
+                return firstNumber;
+            }
+
+            set
+            {
+                if (firstNumber != value)
+                {
+                    firstNumber = value;
+                    OnPropertyChanged(nameof(FirstNumber));
+                    OnPropertyChanged(nameof(Sum));
+                }
+            }
+        }
+
+        public int SecondNumber
+        {
+            get
+            {
+                return secondNumber;
+            }
+            
+            set
+            {
+                if (secondNumber != value)
+                {
+                    secondNumber = value;
+                    OnPropertyChanged(nameof(SecondNumber));
+                    OnPropertyChanged(nameof(Sum));
+                }
+            }
+        }
+
+        public int Sum
+        {
+            get
+            {
+                return firstNumber + secondNumber;
+            }
+        }
+    }
+}
---
+++ CalculatorMiniProject/CalculatorMiniProject/ViewModel/CalculatorViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using CalculatorMiniProject.Model;
+
+
+namespace CalculatorMiniProject.ViewModel
+{
+    public class CalculatorViewModel
+    {
+        public ObservableCollection<CalculatorModel> CalculatorModels
+        {
+            get;
+            set;
+        }
+
+        public void loadModels()
+        {
+            ObservableCollection<CalculatorModel> calculatorModels = new ObservableCollection<CalculatorModel>();
+
+            calculatorModels.Add(new CalculatorModel { FirstNumber = 1, SecondNumber = 2 });
+
+            CalculatorModels = calculatorModels;
+        }
+    }
+}
