34a3431b27389c34b0a5cff97f2f7ad16afa019c
Duc Nguyen <31971333+minhducubc97@users.noreply.github.com>
2018-06-14 20:06:04 -0700

    Add files via upload

---
+++ DispatcherTimerSample/DispatcherTimerSample/Model/DispatcherTimerModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace DispatcherTimerSample.Model
+{
+    class DispatcherTimerModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        private string _dateTimeValue;
+        public string DateTimeValue
+        {
+            get { return _dateTimeValue; }
+            set
+            {
+                _dateTimeValue = value;
+                OnPropertyChanged(nameof(DateTimeValue));
+            }
+        }
+    }
+}
---
+++ DispatcherTimerSample/DispatcherTimerSample/ViewModel/DispatcherTimerViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Threading;
+using DispatcherTimerSample.Model;
+
+namespace DispatcherTimerSample.ViewModel
+{
+    public class DispatcherTimerViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        DispatcherTimerModel dispatcherTimerModel;
+
+        public string DateTimeValue
+        {
+            get { return dispatcherTimerModel.DateTimeValue; }
+            set
+            {
+                dispatcherTimerModel.DateTimeValue = value;
+                OnPropertyChanged(nameof(DateTimeValue));
+            }
+        }
+        public DispatcherTimerViewModel()
+        {
+            dispatcherTimerModel = new DispatcherTimerModel {
+                DateTimeValue = DateTime.Now.ToString("HH:mm:ss.fff")
+            };
+            createDispatcherTimer();
+        }
+
+        public void createDispatcherTimer()
+        {
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Interval = TimeSpan.FromMilliseconds(1);
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dispatcherTimer.Start();
+        }
+
+        public void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            DateTimeValue = DateTime.Now.ToString("HH:mm:ss.fff");
+        }
+    }
+}
