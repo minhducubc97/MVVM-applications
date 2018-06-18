49ababa20b7b7f02ae7137e736ebd1ece56676d7
Duc Nguyen <31971333+minhducubc97@users.noreply.github.com>
2018-06-18 09:47:20 -0700

    Add files via upload

---
+++ CustomCommand/CustomCommand/Model/IDataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomCommand.Model
+{
+    public interface IDataService
+    {
+        void GetData(Action<DataItem, Exception> callback);
+    }
+}
---
+++ CustomCommand/CustomCommand/ViewModel/MainViewModel.cs
@@ -0,0 +1,127 @@
+﻿using GalaSoft.MvvmLight;
+using CustomCommand.Model;
+using System.Windows.Input;
+using System.Windows;
+using GalaSoft.MvvmLight.Command;
+
+namespace CustomCommand.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// See http://www.mvvmlight.net
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly IDataService _dataService;
+
+        /// <summary>
+        /// The <see cref="WelcomeTitle" /> property's name.
+        /// </summary>
+        public const string WelcomeTitlePropertyName = "WelcomeTitle";
+
+        private ICommand hiButtonCommand;
+        private ICommand toggleExecuteCommand;
+        private bool canExecute = true;
+
+        /// <summary>
+        /// Gets the WelcomeTitle property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public string HiButtonContent
+        {
+            get
+            {
+                return "Click to see something cool!";
+            }
+        }   
+        
+        public bool CanExecute
+        {
+            get
+            {
+                return this.canExecute;
+            }
+            set
+            {
+                if (this.canExecute != value)
+                {
+                    this.canExecute = value;
+                }
+            }
+        }
+
+        public ICommand ToggleExecuteCommand
+        {
+            get
+            {
+                return toggleExecuteCommand;
+            }
+            set
+            {
+                toggleExecuteCommand = value;
+            }
+        }
+
+        public ICommand HiButtonCommand
+        {
+            get
+            {
+                return hiButtonCommand;
+            }
+            set
+            {
+                hiButtonCommand = value;
+            }
+        }
+
+        public RelayCommand<Window> Close { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel(IDataService dataService)
+        {
+            _dataService = dataService;
+            _dataService.GetData(
+                (item, error) =>
+                {
+                    if (error != null)
+                    {
+                        // Report error here
+                        return;
+                    }
+                });
+
+            HiButtonCommand = new RelayCommand(ShowMessage, param => this.canExecute);
+            ToggleExecuteCommand = new RelayCommand(ChangeCanExecute);
+            Close = new RelayCommand<Window>(this.closeWindow);
+        }
+
+        public void ShowMessage(object obj)
+        {
+            MessageBox.Show(obj.ToString());
+        }
+
+        public void ChangeCanExecute(object obj)
+        {
+            canExecute = !canExecute;
+        }
+
+        public void closeWindow(Window window)
+        {
+            if (window != null)
+            {
+                window.Close();
+            }
+        }
+
+        ////public override void Cleanup()
+        ////{
+        ////    // Clean up if needed
+
+        ////    base.Cleanup();
+        ////}
+    }
+}
