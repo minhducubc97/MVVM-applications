f112506f567139b232cd97e8b8b61ba669e147c4
Duc Nguyen <31971333+minhducubc97@users.noreply.github.com>
2018-06-25 17:05:50 -0700

    Add files via upload

---
+++ PraceXml/PraceXml/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace PraceXml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlReader xmlReader = XmlReader.Create("../../Xml/Book.xml");
+            while(xmlReader.Read())
+            {
+                if (xmlReader.IsStartElement())
+                {
+                    // return only when you have START tag
+                    switch(xmlReader.Name.ToString())
+                    {
+                        case "author":
+                            Console.WriteLine("Author: " + xmlReader.ReadElementContentAsString());
+                            break;
+                        case "title":
+                            Console.WriteLine("Title: " + xmlReader.ReadElementContentAsString());
+                            break;
+                        case "genre":
+                            Console.WriteLine("Genre: " + xmlReader.ReadElementContentAsString());
+                            break;
+                        case "price":
+                            Console.WriteLine("Price: " + xmlReader.ReadElementContentAsString());
+                            break;
+                        case "publish_date":
+                            Console.WriteLine("Publish Date: " + xmlReader.ReadElementContentAsDateTime().ToShortDateString());
+                            break;
+                        case "description":
+                            Console.WriteLine("Publish Date: " + xmlReader.ReadElementContentAsString());
+                            break;
+                    } 
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
