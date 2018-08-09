53b18dd03e18c3f49adee9fb3cc7f7ad9c3ab9fc
Duc Nguyen <31971333+minhducubc97@users.noreply.github.com>
2018-08-08 22:42:41 -0700

    Add files via upload

---
+++ ReaderWriter/ReaderWriter/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Windows;
+using ReaderWriter.ViewModel;
+
+namespace ReaderWriter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Variables.
+        /// </summary>
+        SpeechRecognizer speechRecognizer;
+
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            speechRecognizer = new SpeechRecognizer();
+        }
+
+        private void btnWriter_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            btnWriter.Height = btnWriter.Height + 20;
+            btnWriter.Width = btnWriter.Width + 20;
+        }
+
+        private void btnWriter_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            btnWriter.Height = btnWriter.Height - 20;
+            btnWriter.Width = btnWriter.Width - 20;
+        }
+
+        private void btnReader_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            btnReader.Height = btnReader.Height + 20;
+            btnReader.Width = btnReader.Width + 20;
+        }
+
+        private void btnReader_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            btnReader.Height = btnReader.Height - 20;
+            btnReader.Width = btnReader.Width - 20;
+        }
+
+        private void ReadMethod(object sender, RoutedEventArgs e)
+        {
+            mainTab.TabIndex = mainTab.TabIndex + 2;
+        }
+
+        private void WriteMethod(object sender, RoutedEventArgs e)
+        {
+            mainTab.TabIndex++;
+        }
+
+        private void startWriting_Click(object sender, RoutedEventArgs e)
+        {
+            content.Text = "";
+        }
+
+        private void Go_Back(object sender, RoutedEventArgs e)
+        {
+            mainTab.TabIndex = 0;
+        }
+
+        private void startListenning_Click(object sender, RoutedEventArgs e)
+        {
+            PromptBuilder promptBuilder = new PromptBuilder();
+            promptBuilder.AppendText("My name is Duc Nguyen");
+
+            PromptStyle promptStyle = new PromptStyle();
+            promptStyle.Volume = PromptVolume.Soft;
+            promptStyle.Rate = PromptRate.Slow;
+            promptBuilder.StartStyle(promptStyle);
+            promptBuilder.AppendText("and I am pleased to meet you.");
+            promptBuilder.EndStyle();
+
+            promptBuilder.AppendText("Today, ");
+            promptBuilder.AppendTextWithHint(DateTime.Now.ToShortDateString(), SayAs.Date);
+
+            promptBuilder.AppendText("I would like to show you this project");
+            promptBuilder.AppendText("about", PromptEmphasis.Strong);
+            promptBuilder.AppendTextWithHint("Listenning and Reading in WPF.", SayAs.SpellOut);
+
+            SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+            speechSynthesizer.Speak(promptBuilder);
+        }
+    }
+}
---
+++ ReaderWriter/ReaderWriter/ViewModel/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using GalaSoft.MvvmLight;
+using ReaderWriter.Model;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ReaderWriter.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// See http://www.mvvmlight.net
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel(IDataService dataService)
+        {
+            InitializeCommand();
+        }
+
+        /// <summary>
+        /// Helpers.
+        /// </summary>
+        /// 
+        private void InitializeCommand()
+        {
+
+        }
+
+        /// <summary>
+        /// Variables declaration.
+        /// </summary>
+
+        /// <summary>
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        /// 
+
+        ////public override void Cleanup()
+        ////{
+        ////    // Clean up if needed
+
+        ////    base.Cleanup();
+        ////}
+    }
+}
